aaca18abd9459175ad65f0e024dd2475bfc6c3cc
Phil K <pkemp21@gmail.com>
2023-04-09 23:39:34 -0700

    More functions

--- MealPlanner/functions.py
+++ MealPlanner/functions.py
@@ -1,26 +1,49 @@
+from multiprocessing.sharedctypes import Value
 import requests
 from bs4 import BeautifulSoup as bs
 
-def get_hf_meal(site):
+def get_meal(site):
     #TODO Hello fresh and BA in same function
-    page = requests.get(site)
-    soup = bs(page.content, 'html.parser')
+    page        = requests.get(site)
+    soup        = bs(page.content, 'html.parser')
+    ingredientList = []
 
-    imgUrl = soup.find(class_='fela-_14dtxzo') #Get image url TODO 
-    for x in imgUrl:
-        print(x)
-    print(soup.title.string.split('|')[0]) #Get title
-    temp2 = soup.find_all('span',{'class':'fela-_36rlri'}) #Get Tags
-    if temp2:
-        for x in temp2:
+    imgUrl = soup.find(class_='fela-_14dtxzo') #Get image url TODO decide which resolution to grab(Split by space (width???), check length pick correct size)
+    
+    # mealTitle = soup.title.string.split('|')[0] #Get title TODO Delete if not needed
+
+    tags = soup.find_all('span',{'class':'fela-_36rlri'}) #Get Tags
+    if tags:
+        for x in tags:
             print(x.get_text().strip('•'))
+    
+    ingredients = soup.find_all("p",{'class':"dsbz dsct dsfs dsbn dsbp dsbq dsft"}) #Get ingredients
+    print(ingredients)
+    try:
+        for x in range(len(ingredients[::2])):
+            ingredientList.append((ingredients[x].get_text(),ingredients[x+1].get_text()))
+    except(ValueError):
+        return
 
-    # temp = soup.find_all("p",{'class':"dsbz dsct dsfs dsbn dsbp dsbq dsft"}) #Get ingredients
-    # for x in temp:
-        # print(x.get_text())
-    #Meal dict - title(str), ingredients(dict?), meal url + image url, tags
+    print(ingredientList)
+    # Meal dict - title(str), ingredients(dict?), meal url + image url, tags
+        
+    # meal = {
+    #     'title'      : soup.title.string.split('|')[0],
+    #     'Ingredients': ingredients,
+    #     'imgUrl'     : soup.find(class_='fela-_14dtxzo'),
+    #     'mealUrl'    : site,
+    #     'tags'       : tags
+    # }
 
-get_hf_meal('https://www.hellofresh.com/recipes/cheese-tortelloni-in-a-mushroom-sauce-5d893112c6d5102c1923bdc5')
+    # print(meal)
+    
+    # return meal
+    return
+
+    
+
+get_meal('https://www.hellofresh.com/recipes/cheese-tortelloni-in-a-mushroom-sauce-5d893112c6d5102c1923bdc5')
 
 def normalize_units():
     #TODO convert ingredients to normalized value (Grams?)
